658194d2237aa47632d09950d2a95d9560275875
JasonPinto24 <pintojason32@gmail.com>
2026-03-20 12:46:47 +0530

    Fixed fetcher and removed secrets

--- src/github_analyzer/fetcher.py
+++ src/github_analyzer/fetcher.py
@@ -1,6 +1,22 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 TOKEN = os.getenv("GITHUB_TOKEN")
+
+def get_repos(username):
+    url = f"https://api.github.com/users/{username}/repos"
+
+    headers = {}
+    if TOKEN:
+        headers["Authorization"] = f"token {TOKEN}"
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print("Error:", response.json())
+        return []
+
+    return response.json()
